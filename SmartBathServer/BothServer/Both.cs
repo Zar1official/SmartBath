29df277aacb1ae55a19dbebae287eae5f98510bb
Zar <87152110+Zar1official@users.noreply.github.com>
2022-04-08 15:33:03 +0300

    upload server

---
+++ SmartBathServer/BothServer/Both.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BothServer
+{
+    public class Both
+    {
+        /// <summary>
+        /// id ванны
+        /// </summary>
+        [JsonPropertyName("uId")]
+        public long uId { get; set; }
+
+        /// <summary>
+        /// температура воды
+        /// </summary>
+        [JsonPropertyName("temp")]
+        public float? temp { get; set; } = null;
+
+        /// <summary>
+        /// набирается ли вода
+        /// </summary>
+        [JsonPropertyName("craneActie")]
+        public bool? craneActie { get; set; } = null;
+
+        /// <summary>
+        /// открыт ли слив
+        /// </summary>
+        [JsonPropertyName("drainStatus")]
+        public bool? drainStatus { get; set; } = null;
+
+        /// <summary>
+        /// цвет воды
+        /// </summary>
+        [JsonPropertyName("waterColor")]
+        public Color? waterColor { get; set; } = null;
+
+        /// <summary>
+        /// на сколько процентов ванна заполнена
+        /// </summary>
+        [JsonPropertyName("fillingProcent")]
+        public float? fillingProcent { get; set; } = null;
+
+        public struct Constants
+        {
+            public static float MinTemp = 5;
+            public static float MaxTemp = 65;
+            public static float SpeedFillingInSeconds = 0.1f;
+
+            /// <summary>
+            /// на сколько заполнилась с предыдущего запроса
+            /// </summary>
+            /// <param name="strartTime">время предыдущего запроса</param>
+            /// <param name="nowTime">текущее время</param>
+            /// <param name="drainStatus">открыт ли слив</param>
+            /// <param name="craneActie">открыт ли кран</param>
+            /// <returns></returns>
+            public static float UpdateFillBothInfo(long strartTime, long nowTime, bool drainStatus, bool craneActie)
+            {
+                float totalSeconds = (float)TimeSpan.FromTicks(nowTime - strartTime).TotalSeconds;
+                if (drainStatus)
+                {
+                    if (craneActie)
+                        return 0;
+                    else
+                        return -(totalSeconds * Both.Constants.SpeedFillingInSeconds);
+                }
+                else
+                {
+                    if (craneActie)
+                        return (totalSeconds * Both.Constants.SpeedFillingInSeconds);
+                    else
+                        return 0;
+                }
+            }
+        }
+
+        [JsonPropertyName("timeStamp")]
+        public long timeStamp { get; set; }
+
+    }
+
+    /// <summary>
+    /// подсветка воды
+    /// </summary>
+    public enum Color
+    {
+        Red,
+        White, 
+        Blue
+    }
+}
---
+++ SmartBathServer/BothServer/Controllers/MyBoth.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BothServer.Controllers
+{
+    [ApiController]
+    [Route("api/getBoth")]
+    public class MyBoth : ControllerBase
+    {
+        [HttpGet]
+        public Both? GetMyBoth(long uId = 0)
+        {
+            if (uId == 0)
+                return null;
+            JsonManager manager = new JsonManager(uId);
+            Both myBoth = manager.GetMyBoth();
+
+            long time = myBoth.timeStamp;
+            long nowTime = DateTime.Now.Ticks;
+
+            myBoth.fillingProcent += Both.Constants.UpdateFillBothInfo(time, nowTime, (bool)myBoth.drainStatus, (bool)myBoth.craneActie);
+
+            if (myBoth.fillingProcent < 0)
+                myBoth.fillingProcent = 0;
+            if (myBoth.fillingProcent > 100)
+                myBoth.fillingProcent = 100;
+
+            myBoth.timeStamp = nowTime;
+
+            return myBoth;
+        }
+    }
+}
